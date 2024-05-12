9bfd8dd23409a8d13aa92f1da4bc565542c1fbac
Wilian Cirillo <me@wilian.io>
2024-05-12 18:03:18 -0300

    Change log

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='tree-viewer',
-   version='0.1-dev',
+   version='0.1.0',
    packages=find_packages(),
    description="Tree-Viewer is a Python library designed to generate customizable visual representations of directory structures, enabling users to clearly and efficiently organize and manage their project layouts. With its user-friendly interface, Tree-Viewer provides a comprehensive view of your directories, presenting all necessary details in a structured and easily accessible format. This tool is ideal for developers seeking to maintain an intuitive overview of complex projects, ensuring all components are logically arranged for optimal navigation and project oversight.",
    long_description=open('README.md', encoding='utf-8').read(),
